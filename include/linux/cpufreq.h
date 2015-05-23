1a5deb638f0f2c103a55e8c7869c760027ff8c1e
flar2 <asegaert@gmail.com>
2015-05-23 11:48:55 -0400

    cpufreq_elementalx: add missing declaration
    For configuring elementalx as default governor.
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -447,6 +447,9 @@ extern struct cpufreq_governor cpufreq_gov_cafactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
+extern struct cpufreq_governor cpufreq_gov_elementalx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
 #endif
 
 /*********************************************************************
