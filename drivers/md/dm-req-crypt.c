ef67cb22e07c33ade03e7c3e9272f6b7484429b3
Dinesh K Garg <dineshg@codeaurora.org>
2015-10-27 12:23:11 -0700

    md: dm-req-crypt: Initialize auto variable
    req_crypt_should_encrypt() and req_crypt_should_deccrypt() APIs are
    using variable ret without initialization. This could cause these APIs
    to return wrong values. Initialized ret with proper value.
    
    Change-Id: I42ec8c12f02bf22b0434c3931842609cd9a8b8e0
    Signed-off-by: Dinesh K Garg <dineshg@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/md/dm-req-crypt.c
+++ drivers/md/dm-req-crypt.c
@@ -153,7 +153,7 @@ static  unsigned int get_sg_len(struct request *rq)
 
 static  bool req_crypt_should_encrypt(struct req_dm_crypt_io *req)
 {
-	int ret;
+	int ret = 0;
 	bool should_encrypt = false;
 	struct bio *bio = NULL;
 	u32 key_id = 0;
@@ -182,7 +182,7 @@ static  bool req_crypt_should_encrypt(struct req_dm_crypt_io *req)
 
 static  bool req_crypt_should_deccrypt(struct req_dm_crypt_io *req)
 {
-	int ret;
+	int ret = 0;
 	bool should_deccrypt = false;
 	struct bio *bio = NULL;
 	u32 key_id = 0;
