31fb60a4c605c042f92597183764e202a4b5d325
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 01:13:45 +0000

    display: add a simple api to query the display state (on/off) at any point in time
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -22,7 +22,7 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
-
+#include <linux/display_state.h>
 #include "mdss_dsi.h"
 
 #if IS_ENABLED(CONFIG_LGE_DISPLAY_POWER_SEQUENCE)
@@ -45,6 +45,17 @@ extern int mdss_dsi_cmdlist_rx(struct mdss_dsi_ctrl_pdata *ctrl,
 				struct dcs_cmd_req *req);
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
+
+static int mdss_bl_ctrl_by_panel(void)
+{
+	return mdss_bl_ctrl_panel;
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -685,6 +696,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -769,6 +782,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	if (lge_mdss_dsi.post_mdss_dsi_panel_off)
 		lge_mdss_dsi.post_mdss_dsi_panel_off(pdata);
 #endif
+	display_on = false;
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
