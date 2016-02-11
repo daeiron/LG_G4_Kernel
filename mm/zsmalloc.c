acc1a483058ca08af3c8331d8b7724d6eaef7900
Thomas van Gerven <thomas1991_12@hotmail.com>
2016-02-11 12:34:53 +0100

    Fix in zsmalloc.c

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -368,7 +368,7 @@ static void zs_zpool_unmap(void *pool, unsigned long handle)
 
 static u64 zs_zpool_total_size(void *pool)
 {
-	return zs_get_total_size_bytes(pool);
+	return zs_get_total_pages(pool) << PAGE_SHIFT;
 }
 
 static struct zpool_driver zs_zpool_driver = {
