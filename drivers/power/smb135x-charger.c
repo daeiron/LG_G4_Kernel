25e5fb9235d5348d9fb151848bd6f22443b96fdd
Thomas van Gerven <thomas1991_12@hotmail.com>
2016-02-12 19:26:30 +0100

    Revert "added USB Fast Charge"
    This reverts commit b4684573fde30eb46fa3844b9a89089c0c035dc5.

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -32,10 +32,6 @@
 #ifdef CONFIG_LGE_PM_FACTORY_PSEUDO_BATTERY
 #include <soc/qcom/lge/board_lge.h>
 #endif
-
-#ifdef CONFIG_FORCE_FAST_CHARGE
-#include <linux/fastcharge.h>
-#endif
 #define SMB135X_BITS_PER_REG	8
 
 /* Mask/Bit helpers */
@@ -1217,10 +1213,6 @@ static int smb135x_set_high_usb_chg_current(struct smb135x_chg *chip,
 	return rc;
 }
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-extern int force_fast_charge;
-#endif
-
 #define MAX_VERSION			0xF
 #define USB_100_PROBLEM_VERSION		0x2
 /* if APSD results are used
@@ -1279,12 +1271,7 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 #ifdef CONFIG_LGE_PM_PARALLEL_CHARGING
 #else
 	if (current_ma == CURRENT_500_MA) {
-
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
-#else
 		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
-#endif
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
 		rc |= smb135x_path_suspend(chip, USB, CURRENT, false);
@@ -2235,11 +2222,7 @@ static void smb135x_external_power_changed(struct power_supply *psy)
 
 	if (chip->usb_psy_ma != current_limit) {
 		mutex_lock(&chip->current_change_lock);
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		chip->usb_psy_ma = 1600;
-#else
 		chip->usb_psy_ma = current_limit;
-#endif
 		rc = smb135x_set_appropriate_current(chip, USB);
 		mutex_unlock(&chip->current_change_lock);
 		if (rc < 0)
