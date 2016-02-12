deed95aab5ef7402f006f4894260fc67fca25492
Thomas van Gerven <thomas1991_12@hotmail.com>
2016-02-12 21:07:27 +0100

    Add zzmoove

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,6 +444,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmove;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmove)
 #endif
 
 /*********************************************************************
